a95a6d6a763e9e6515cbdf246caa77c32ce90c88
elliottsmith1 <elliottsmith1@hotmail.co.uk>
2018-02-19 15:00:29 +0000

    started file management

---
+++ Assets/Scripts/FileManagement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class FileManagement : MonoBehaviour
+{
+    string current_name = "Filename";
+    int[] map_data = new int[2];
+
+    void Start()
+    {
+        SaveFile();
+        LoadFile();
+    }
+
+    public void SaveFile()
+    {
+        string destination = Application.persistentDataPath + "/save.dat";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenWrite(destination);
+        else file = File.Create(destination);
+
+        MapData data = new MapData(current_name, map_data);
+        BinaryFormatter bf = new BinaryFormatter();
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void LoadFile()
+    {
+        string destination = Application.persistentDataPath + "/save.dat";
+        FileStream file;
+
+        if (File.Exists(destination)) file = File.OpenRead(destination);
+        else
+        {
+            Debug.LogError("File not found");
+            return;
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+        MapData data = (MapData)bf.Deserialize(file);
+        file.Close();
+
+        current_name = data.name;
+        map_data = data.map;
+
+        Debug.Log(data.name);
+        Debug.Log(data.map);
+    }
+
+}
---
+++ Assets/Scripts/MapData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class MapData
+{
+    public string name;
+    public int[] map;
+
+    public MapData(string _name, int[] _map)
+    {
+        name = _name;
+        map = _map;
+    }
+}
